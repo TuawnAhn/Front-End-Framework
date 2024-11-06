5b89ebd6c2c61956f76ce3160e6fbfb0aa6dd35a
Tuấn Anh <your-email@example.com>
2024-11-06 20:22:49 +0700

    vue-js

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,102 @@
-<script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-import { ref } from 'vue'
-const className = ref('Chào mừng đến với WD19312')
-const products = [
-  {
-    name: 'Sản phẩm 1',
-    description: 'Mô tả sản phẩm 1, rất chất lượng và đáng mua.',
-    price: 500000,
-    image:
-      'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
-  },
-  {
-    name: 'Sản phẩm 2',
-    description: 'Mô tả sản phẩm 2, rất chất lượng và đáng mua.',
-    price: 500000,
-    image:
-      'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
-  },
-  {
-    name: 'Sản phẩm 3',
-    description: 'Mô tả sản phẩm 3, rất chất lượng và đáng mua.',
-    price: 500000,
-    image:
-      'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
-  },
-  {
-    name: 'Sản phẩm 4',
-    description: 'Mô tả sản phẩm 4, rất chất lượng và đáng mua.',
-    price: 500000,
-    image:
-      'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
-  },
-  {
-    name: 'Sản phẩm 5',
-    description: 'Mô tả sản phẩm 5, rất chất lượng và đáng mua.',
-    price: 500000,
-    image:
-      'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
-  },
-  {
-    name: 'Sản phẩm 6',
-    description: 'Mô tả sản phẩm 6, rất chất lượng và đáng mua.',
-    price: 500000,
-    image:
-      'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
-  },
-]
-</script>
-
-<!-- <template> -->
-<!-- <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      {{ className }}
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header> -->
-<!-- <div class="container my-5">
-    <div class="row g-4">
-      <div class="col-md-4" v-for="product in products" :key="product.name">
-        <div
-          class="card h-100 shadow-lg border-0 rounded-lg overflow-hidden position-relative card-hover"
-        >
-          <div class="card-header bg-primary text-white text-center py-4">
-            <h1 class="card-title fw-bold">FPT Shop</h1>
-          </div>
-          <div class="card-body text-center">
-            <div class="product-image">
-              <img
-                :src="product.image"
-                class="img-fluid my-3 rounded-circle border shadow-sm"
-                style="width: 150px; height: 150px; object-fit: cover"
-              />
-            </div>
-            <ul class="list-unstyled">
-              <li>
-                <h2 class="fw-semibold text-dark">{{ product.name }}</h2>
-              </li>
-              <li class="text-muted">{{ product.description }}</li>
-            </ul>
-            <div class="price-section">
-              <h3 class="text-primary fw-bold">{{ product.price }} VND</h3>
-            </div>
-            <a href="#" class="btn btn-gradient mt-4 w-100 btn-lg">
-              <i class="bi bi-cart"></i> Mua
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div> -->
-
-<!-- <RouterView /> -->
 <template>
+  <div>
+    <table class="container">
+      <thead>
+        <tr>
+          <th scope="col">Stt</th>
+          <th scope="col">Tên sản phẩm</th>
+          <th scope="col">Mô tả</th>
+          <th scope="col">Price</th>
+          <th scope="col">Image</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(product, index) in products" :key="index">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ product.name }}</td>
+          <td>{{ product.description }}</td>
+          <td>{{ product.price }}</td>
+          <td>
+            <img :src="product.image" alt="" style="width: 120px" />
+          </td>
+          <td>
+            <button class="btn btn-primary" @click="editProduct(index)">
+              Sửa
+            </button>
+            <button class="btn btn-warning" @click="deleteProduct(index)">
+              Xoá
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      products: [
+        {
+          name: 'Sản phẩm 1',
+          description: 'Mô tả sản phẩm 1, rất chất lượng và đáng mua.',
+          price: 500000,
+          image:
+            'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
+        },
+        {
+          name: 'Sản phẩm 2',
+          description: 'Mô tả sản phẩm 2, rất chất lượng và đáng mua.',
+          price: 500000,
+          image:
+            'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
+        },
+        {
+          name: 'Sản phẩm 3',
+          description: 'Mô tả sản phẩm 3, rất chất lượng và đáng mua.',
+          price: 500000,
+          image:
+            'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
+        },
+        {
+          name: 'Sản phẩm 4',
+          description: 'Mô tả sản phẩm 4, rất chất lượng và đáng mua.',
+          price: 500000,
+          image:
+            'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
+        },
+        {
+          name: 'Sản phẩm 5',
+          description: 'Mô tả sản phẩm 5, rất chất lượng và đáng mua.',
+          price: 500000,
+          image:
+            'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
+        },
+        {
+          name: 'Sản phẩm 6',
+          description: 'Mô tả sản phẩm 6, rất chất lượng và đáng mua.',
+          price: 500000,
+          image:
+            'https://inbaobigiay.vn/wp-content/uploads/2024/06/logo-fpt-vector-6.jpg',
+        },
+      ],
+    }
+  },
+  methods: {
+    deleteProduct(index) {
+      if (confirm('Bạn có chắc chắn muốn xóa sản phẩm này không?')) {
+        this.products.splice(index, 1)
+      }
+    },
+    editProduct(index) {
+      alert(`Sửa sản phẩm ${this.products[index].name}`)
+    },
+  },
+}
+</script>
+<!-- <template>
   <div class="product-list">
     <div v-for="(product, index) in products" :key="product.name">
       <div
@@ -111,6 +108,13 @@ const products = [
           <h2>{{ product.name }}</h2>
           <p>{{ product.description }}</p>
           <p>Giá: {{ product.price }} VND</p>
+          <button
+            type="submit"
+            class="btn btn-primary"
+            onclick="confirm('Hello World!')"
+          >
+            Sửa
+          </button>
         </div>
       </div>
 
@@ -123,12 +127,19 @@ const products = [
           <h2>{{ product.name }}</h2>
           <p>{{ product.description }}</p>
           <p>Giá: {{ product.price }} VND</p>
+          <button
+            type="submit"
+            class="btn btn-danger"
+            onclick="confirm('Hello World!')"
+          >
+            Sửa
+          </button>
         </div>
         <img style="width: 300px" :src="product.image" />
       </div>
     </div>
   </div>
-</template>
+</template> -->
 
 <style scoped>
 header {
